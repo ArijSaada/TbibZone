d2e9cb79ee34c394476f61f4813b79ad3ece741e
ArijSaada <arij.saada.dev@gmail.com>
2026-01-30 23:03:47 +0100

    Add files via upload

---
+++ BackOffice/Superuser/TheSuperUser/src/app/edit-admin/edit-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-admin',
+  standalone: false,
+  templateUrl: './edit-admin.component.html',
+  styleUrl: './edit-admin.component.css'
+})
+export class EditAdminComponent {
+
+}
